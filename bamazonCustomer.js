9a28aa20c6de1d4c58ad13d6c7b69d26f2261bc0
Tyler Casperson <tylercasperson@gmail.com>
2020-02-03 22:29:26 -0700

    displays tabular data

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,18 +1,61 @@
+require ("dotenv").config();
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+// var table = require("table");
+const table = require('table').table
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3004,
-    user: "root",
-    password: "root",
+    port: 3306,
+    user: process.env.dbUserName,
+    password: process.env.dbPassword,
     database: "bamazonDB"
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
+    console.log('1');
+    process.on('uncaughtException', function (err) {
+        console.log(err);
+    }); 
     console.log("connected as id " + connection.threadId + "\n");
-})
+    readProducts();
+    // readDepartments();
+    // readEmployees();
+});
 
 // console.log('What is the id of the product you would like to buy?');
 // console.log('How many units of ' + productName + ' would you like to buy?');
 
+function readProducts(){
+    console.log('Selecting all products...\n');
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;     
+        
+        var data = [
+            ['id', 'productName', 'departmentID', 'cost', 'price', 'QOH', 'min', 'max']
+        ]
+        for(var i =0; i<res.length;i++){        
+            data.push([res[i].id, res[i].productName, res[i].deptID, res[i].cost, res[i].price, res[i].quantityOnHand, res[i].minLevel, res[i].maxLevel],);            
+        }        
+        console.log(table(data));
+        connection.end();
+    });
+}
+
+function readDepartments(){
+    console.log('Selecting all departments...\n');
+    connection.query("SELECT * FROM departments", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+}
+
+function readEmployees(){
+    console.log('Selecting all Employees...\n');
+    connection.query("SELECT * FROM employees", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+}
---
+++ seed.sql
@@ -0,0 +1,76 @@
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;  
+create table products (
+	id INT NOT NULL AUTO_INCREMENT,
+    productName varchar(100) NOT NULL,
+    deptID INT NOT NULL,
+    cost float(10,3) NOT NULL,
+    price float(10,3) NOT NULL,
+    quantityOnHand INT,
+    minLevel INT ,
+    maxLevel INT ,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO products (productName, deptID, cost, price, quantityOnHand, minLevel, maxLevel)
+VALUES ("Computer", 1, 100.32, 499.99, 25, 5, 50);
+
+INSERT INTO products (productName, deptID, cost, price, quantityOnHand, minLevel, maxLevel)
+VALUES ("xbox", 1, 50.25, 399.99, 40, 100, 500);
+
+INSERT INTO products (productName, deptID, cost, price, quantityOnHand, minLevel, maxLevel)
+VALUES ("Super Mario Brothers", 1, 1000.84, 500000.00, 6, 3, 300);
+
+INSERT INTO products (productName, deptID, cost, price, quantityOnHand, minLevel, maxLevel)
+VALUES ("Mario Kart", 1, 500.00, 1299.99, 80, 10, 100);
+
+INSERT INTO products (productName, deptID, cost, price, quantityOnHand, minLevel, maxLevel)
+VALUES ("BlueNecklace", 4, 2.00, 59.99, 10, 2, 10);
+
+INSERT INTO products (productName, deptID, cost, price, quantityOnHand, minLevel, maxLevel)
+VALUES ("CrystalBlueRing", 4, 1.00, 29.99, 50, 10, 30);
+
+
+USE bamazonDB;    
+CREATE TABLE employees (
+	id INT NOT NULL AUTO_INCREMENT,
+    employeeName VARCHAR(100) NOT NULL,
+    managerID INT NOT NULL,
+    PRIMARY KEY (id) 
+);
+    
+INSERT INTO employees(employeeName, managerID)
+VALUES("Tyler", 1);
+
+INSERT INTO employees(employeeName, managerID)
+VALUES("Steve", 1);
+
+INSERT INTO employees(employeeName, managerID)
+VALUES("Bob", 1);
+
+INSERT INTO employees(employeeName, managerID)
+VALUES("Emily", 4);
+
+INSERT INTO employees(employeeName, managerID)
+VALUES("Eric", 4);
+
+INSERT INTO employees(employeeName, managerID)
+VALUES("Richard", 4);
+
+USE bamazonDB;  
+CREATE TABLE departments (
+	id int NOT NULL AUTO_INCREMENT,
+    departmentName VARCHAR(50) NOT NULL,
+    managerID INT NOT NULL,
+    overhead_cost FLOAT(10,3),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO departments (departmentName, managerID, overhead_cost)
+VALUES ("Electronics", 1, 500.50);
+
+INSERT INTO departments (departmentName, managerID, overhead_cost)
+VALUES ("Jewlery", 4, 200.34);
