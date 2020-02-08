85e79b12edb1de9d5fc6cc91dfed67390fe73844
Tyler Casperson <tylercasperson@gmail.com>
2020-02-08 06:38:45 -0700

    asks how many to buy

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -49,8 +49,40 @@ function readDepartments(){
         whichDepartment();
         // connection.end();
     });
-    
 }
+
+function readProduct(department){
+    connection.query("SELECT * FROM products INNER JOIN departments ON departments.departmentID = deptID AND deptID = ?", department, function(err, res) {
+        if(err) throw err;
+        var data = [
+            ['id', 'ProductName', 'Price']
+        ]
+        for(var i =0; i<res.length;i++){        
+            data.push([res[i].productID, res[i].productName, res[i].price],);            
+        }        
+        console.log(table(data));
+    });
+}
+
+function specificProduct(thisOne){
+    connection.query("SELECT * FROM products WHERE productID =" + thisOne[Object.keys(thisOne)[0]], function(err, res) {
+        if(err) throw err;
+        var data = [
+            ['id', 'ProductName', 'Price']
+        ]
+        for(var i =0; i<res.length;i++){        
+            data.push([res[i].productID, res[i].productName, res[i].price]);            
+        }        
+        console.log(table(data));
+    });
+}
+
+function inventoryCheck(qty){
+
+
+}
+
+
 
 function whichDepartment(){
     inquirer.prompt({
@@ -62,22 +94,80 @@ function whichDepartment(){
             var departmentDecision = departmentResponse.whichDepartment;
             connection.query("SELECT * FROM products INNER JOIN departments ON departments.departmentID = deptID AND deptID = ?", departmentDecision, function(err, res) {
                 if (err) throw err;
+            
                 if(res.length === 0){
                     console.log('The selection is not valid. Please enter a department on the list.');
                     whichDepartment();
                 } else {
-                    var data = [
-                        ['id', 'ProductName', 'Price']
-                    ]
-                    for(var i =0; i<res.length;i++){        
-                        data.push([res[i].productID, res[i].productName, res[i].price],);            
-                    }        
-                    console.log(table(data));
-                    connection.end();
-                }
+                    readProduct(departmentDecision);
+                    inquirer.prompt({
+                        name: "whichProduct",
+                        type: "input",
+                        message: "Which product would you like to purchase?"
+                    })
+                    .then(function(productResponse){
+                        specificProduct(productResponse);
+                        inquirer.prompt({
+                            name: "howMany",
+                            type: "input",
+                            message: "How many would you like to purchase?"
+                        })
+                        .then(function(qunatity){
+                            var qty = qunatity.howMany;
+                            console.log(qty);
+                            if (qty > res[0].quantityOnHand){
+                                console.log("I am sorry but we only have " + res[0].quantityOnHand + " in stock.")
+                            }
+                            console.log('hello');
+
+                        })
+
+
+
+                    });
+
+
+                    } //end of else
             });
-        });
-};
+        })
+    };
+    
+    
+                        
+          
+
+
+
+
+// function whichProduct(){
+//     inquirer.prompt({
+//             name: "whichProduct",
+//             type: "input",
+//             message: "Which product would you like to purchase? Please select an id."
+//         })
+//         .then(function(productResponse){
+//             var departmentDecision = departmentResponse.whichDepartment;
+//             var productDecision = productResponse.whichProduct;
+//             connection.query("SELECT * FROM products INNER JOIN departments ON departments.departmentID = deptID AND deptID = ? AND productID = ?", departmentDecision, productDecision, function(err, res) {
+//                 if (err) throw err;
+//                 if(res.length === 0){
+//                     console.log('The selection is not valid. Please enter a product on the list.');
+//                     whichProduct();
+//                 } else {
+//                     console.log('hello');
+//                     // var data = [
+//                     //     ['id', 'ProductName', 'Price']
+//                     // ]
+//                     // for(var i =0; i<res.length;i++){        
+//                     //     data.push([res[i].productID, res[i].productName, res[i].price],);            
+//                     // }        
+//                     // console.log(table(data));
+//                     // connection.end();
+//                 }
+//             });
+//         });
+// };
+
 
 
 
